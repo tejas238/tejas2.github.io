9a7ca1066ab9ae2ad9666559aa20fd655eebc819
Tejas Menon <tejas.menon@hotmail.com>
2020-02-04 14:49:30 -0800

    added animations

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 window.onload = function() {   
     document.getElementById("aboutmecircle").onclick = function() {
-        document.getElementById("aboutmepath").classList.add("animatelinemove");
+        path = document.getElementById("aboutmepath").classList.add("animatelinemove");
+        document.getElementById("aboutmecircle").style.visibility = "hidden";
+        document.getElementById("aboutme").style.visibility = "visible";
+        document.getElementById("aboutme").style.animation = "fadein 2s";
+
     }
     
     document.getElementById("contactmecircle").onclick = function() {
-            document.getElementById("contactmepath").classList.add("animatelinemove");
+            path = document.getElementById("contactmepath").classList.add("animatelinemove");
+            document.getElementById("contactmecircle").style.visibility = "hidden";
+            document.getElementById("contact").style.visibility = "visible";
+            document.getElementById("contact").style.animation = "fadein 2s";
     }
     
     document.getElementById("linkscircle").onclick = function() {
-            document.getElementById("linkspath").classList.add("animatelinemove");
+            path = document.getElementById("linkspath").classList.add("animatelinemove");
+            document.getElementById("linkscircle").style.visibility = "hidden";
+            document.getElementById("links").style.visibility = "visible";
+            document.getElementById("links").style.animation = "fadein 2s";
     }
 
     document.getElementById("projectscircle").onclick = function() {
-        document.getElementById("projectspath").classList.add("animatelinemove");
-}
+        path = document.getElementById("projectspath").classList.add("animatelinemove");
+        document.getElementById("projectscircle").style.visibility = "hidden";
+        document.getElementById("projects").style.visibility = "visible";
+        document.getElementById("projects").style.animation = "fadein 2s";
+    }
 }
